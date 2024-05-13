f196e763689dc14cab84b37abf13ceeffa650e76
Nicoya505 <figueroablanco92@gmail.com>
2024-05-13 22:32:04 +0200

    refactor(error-handler): update-error-handler-in-main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from config import Base, engine
 from routers import movie_router, user_router
+from middlewares import ErrorHandler
 
 from fastapi import FastAPI
 
 app = FastAPI()
 app.title = "Movies ApiRest"
 app.version = "0.0.1"
+
+app.add_middleware(ErrorHandler)
 
 app.include_router(movie_router)
 app.include_router(user_router)
