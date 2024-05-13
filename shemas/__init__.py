992801fb48a48719e6d2b99cafec574956b474cd
Nicoya505 <figueroablanco92@gmail.com>
2024-05-13 20:56:46 +0200

    fea(api):add-movie-shema

---
+++ shemas/__init__.py
@@ -0,0 +1,1 @@
+from .movie_shema import MovieShema
---
+++ shemas/movie_shema.py
@@ -0,0 +1,9 @@
+
+from pydantic import BaseModel
+from pydantic import Field
+
+class MovieShema(BaseModel):
+    titulo:str = Field(..., min_length=5, max_length=50)
+    category:str = Field(..., min_length=5, max_length=50)
+    year:int = Field(..., gt=1990)
+    
